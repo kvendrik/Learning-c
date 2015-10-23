fc84517d6a0a5caafb89bd4068538c4ef0dd2587
Matti van de Weem <mattiweem@hotmail.com>
2015-10-23 13:09:18 +0200

    learning c and stuff

---
+++ ex3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+
+	int length = 180;
+	int age = 20;
+
+	char name[] = "Matti";
+
+	printf("My name is %s \n", name);
+	printf("i'm %d years old \n", age);
+	printf("my length = %d cm \n", length);
+
+	return 0;
+}
